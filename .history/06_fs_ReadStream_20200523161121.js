f4a6390b5371e9dc3a366e9e3d6f284d5f369539
huoliwei <15603379400@163.com>
2020-05-29 09:53:15 +0800

    删除在公司写的读取流文件脚本

--- .history/06_fs_ReadStream_20200523161121.js
+++ .history/06_fs_ReadStream_20200523161121.js
@@ -9,4 +9,14 @@ let str = ''
 fileReadStream.on('data',(chunk)=>{
     console.log(`${++count}接收到:${chunk.length}`);
     
+    str+=chunk
 })
+
+fileReadStream.on('end',()=>{
+    console.log('结束');
+    console.log(count);
+    console.log(str);
+    
+})
+
+fileReadStream.on('error',
